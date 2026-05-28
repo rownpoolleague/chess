3fe732160441c4193ddbd900450216088157690f
rownpoolleague <rownpoolleague@gmail.com>
2026-05-27 21:59:17 -0500

    Update asfa.js

--- asfa.js
+++ asfa.js
@@ -1,3 +1,3 @@
 const APP_CONFIG = {
-    APPSCRIPT_URL: "https://script.google.com/macros/s/AKfycbzTmJRN7G3ee0Svp5NaO41kygaxQTGqV69W11yRNk44Dtr2jRJg5Ba9j_DiZHeGdmHwmQ/exec"
+    APPSCRIPT_URL: "https://script.google.com/macros/s/AKfycbyDC8dpNYxwwzql6usyp4vhY5cBtmju_tjRsX3PbD0BTvQlQlu6N3xy6jRFpcmR_aU4mw/exec"
 };
